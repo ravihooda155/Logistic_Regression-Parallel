48786fae29836fd89bea323691813184938bad5b
neil <nilabja10201992@gmail.com>
2019-04-07 01:08:29 +0530

    added one vs all logistic regression

--- lr.cpp
+++ lr.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <random>
 #include <cmath>
 #include <memory>
 #include <fstream>
 #include "data.h"
 using namespace std;
-class LR {
-public:
-    double** scale(double**x,int m, int n){
+
+float get_random(){
+    static std::default_random_engine e;
+    static std::uniform_real_distribution<> dis(0, 1); // rage 0 - 1
+    return dis(e);
+}
+ double** scale(double**x,int m, int n){
         double** scale_x = dmatrix(m,n);
         for(int i = 0; i < n ;++i){//feature
             double mean = 0.0;
@@ -22,7 +27,9 @@ public:
             }
         }
         return scale_x;
-    }
+}
+class LR {
+public:
     static double inner_prod(const double* v1, const double* v2, int n) {
         double r = 0.0;
         for(int i = 0; i < n; ++i) {
@@ -30,26 +37,11 @@ public:
         }
         return r;
     }
-    double distance(const double* v1, const double* v2, int n) {
-        double sum = 0;
-        for(int i = 0; i < n; ++i) {
-            double minus = v1[i] - v2[i];
-            double r = minus * minus;
-            sum += r;
-        }
-        return sqrt(sum);
-    }
     double sigmoid(double x) {
-        double e = 2.718281828;
-        if (x >= 10){
-            return 1.0 / (1.0 + pow(e, -10));
-        }else if (x <= -10){
-            return 1.0 / (1.0 + pow(e, 10));
-        }
-        return 1.0 / (1.0 + pow(e, -x));
+        return exp(x)/ (1.0 + exp(x));
     }
-    int binary(double* x){
-        return inner_prod(x, _weight_new, _dim) + _bias > 0;
+    double binary(double* x){
+        return sigmoid(inner_prod(x, _weight_new, _dim) + _bias);
     }
     double h(double* x) {
         return h(x, _weight_new, _dim, _bias);
@@ -59,27 +51,32 @@ public:
         return sigmoid(y);
     }
     //y: 0,1
-    double fit(double**nx, int m, int n, double* y, double alpha = 0.01, double l2 = 0.0, double l1=0.0) {
-        int max_iters = 4000;
+    void fit(double**nx, int m, int n, double* y, double alpha = 0.01, double l2 = 0.0, double l1=0.0, int itr = 5000) {
+        int max_iters = itr;
         memset(_weight_old, 0, sizeof(_weight_old[0])*_dim);
         memset(_weight_new, 0, sizeof(_weight_new[0])*_dim);
+        for (int i=0; i <_dim; ++i)
+            _weight_old[i] = get_random();
+        // for(int i=0;i<_dim;i++){
+        //     cout<<_weight_old[i]<<" ";
+        // }
+        //cout<<endl;
         //double** x = scale(nx, m, n);
         double**x = nx;
         double* predict = new double[m];
 	
-        double last_mrse = 1e10;
+        double last_cross_entropy_loss = 0;
         for(int iter = 0; iter < max_iters; ++iter) {
             //predict
             double mrse = 0;
+            double cross_entropy_loss = 0;
             for(int i = 0; i < m; ++i) {
                 predict[i] = h(x[i], _weight_old, _dim, _bias_old);
-                mrse += (y[i] - predict[i]) * (y[i] - predict[i]);
+                //mrse += (y[i] - predict[i]) * (y[i] - predict[i]);
+                cross_entropy_loss += - (y[i]*log(predict[i]) + (1-y[i])*log(1-predict[i]));
             }
-            //cout << "mrse:" << sqrt(mrse/m) << endl;
-            if (last_mrse - mrse < 0.0001){
-                return mrse;
-            }
-            last_mrse = mrse;
+        
+            last_cross_entropy_loss = cross_entropy_loss;
             std::swap(_weight_old, _weight_new);
             _bias = _bias_old;
             //update each weight
@@ -88,7 +85,7 @@ public:
                 for(int i = 0; i < m; ++i) {
                     gradient += (predict[i] - y[i]) * x[i][k];
                 }
-                _weight_new[k] = _weight_old[k] - alpha * gradient/m - l2 * _weight_old[k];
+                _weight_new[k] = _weight_old[k] - (alpha/m) *( gradient + l2 * _weight_old[k]);
                 //if (_weight_new[k] < 11){ _weight_new[k] = 0; }
             }
             //update bias
@@ -96,9 +93,9 @@ public:
             for(int i = 0; i < m; ++i) {
                 g += (predict[i] - y[i]);
             }
-            _bias_old = _bias - alpha * g/m - l2 * _bias;
+            _bias_old = _bias - alpha * g/m;
         }
-        return distance(_weight_new, _weight_old, _dim);
+        //return distance(_weight_new, _weight_old, _dim);
     }
     void save(std::ostream& os) {
         os << "\t"<<"bias:" << _bias << " "<<endl;
@@ -112,10 +109,6 @@ public:
         _bias = 0.0;
         _bias_old = 0.0;
     }
-    ~LR() {
-        delete[] _weight_old;
-        delete[] _weight_new;
-    }
 private:
     double* _weight_old;
     double* _weight_new;
@@ -124,47 +117,85 @@ private:
     double _bias_old;
 };
 int main(int argc, char* argv[]) {
-    if(argc < 4) {
-        cerr << "Usage: " << argv[0] << " <train_feature> <train_target> <rows> <cols> [test]" << endl
+    if(argc < 5) {
+        cerr << "Usage: " << argv[0] << " <train_feature> <train_target> <rows> <cols> <classes> [test]" << endl
              << "\t data_file: the training date\n";
         return -1;
     }
     const char* feature = argv[1];
     const char* target = argv[2];
+    int num_classes = atoi(argv[5]);
     int row = atoi(argv[3]);
     int col = atoi(argv[4]); //add bias
     double**x = dmatrix(row, col);
+    double **scale_x = dmatrix(row, col);
     double* y = dvector(row);
     //load_data(train_instance, x,y);  //if train_target\ttrain_feature are merged in one file
     csv_load_feature(feature, x);
     load_target(target, y);
-    LR model(col);
-    model.fit(x, row, col, y, 0.1);
-    model.save(std::cout);
-    double** confuse = dmatrix(2, 2);
-    for(int i = 0; i < row; ++i) {
-        int pred = model.binary(x[i]);
-        int label = (int)y[i];
-        confuse[label][pred]++;
+    vector<LR> v;
+    scale_x = scale(x,row, col);
+    for (int i = 0;i<num_classes; i++){
+        LR model(col);
+        int n = sizeof(y)/sizeof(y[0]);
+        double* y_temp = dvector(row);
+        for(int j=0;j<n;j++){
+            if(y[j]==i)
+                y_temp[j] = 1;
+            else
+                y_temp[j] = 0;
+        }
+        model.fit(scale_x, row, col, y_temp, 0.001);
+        cout<<i<<endl;
+        //model.save(std::cout);
+        v.push_back(model);
     }
-    cout<<"--------------------"<<endl;
-    cout<<"Confusion Matrix"<<endl;
-    cout<<"--------------------"<<endl;
-    cout << "LABEL\t0\t1\tprecision\tsupprt<-prdict\n";
-    double label0 = confuse[0][0] + confuse[0][1];
-    double label1 = confuse[1][0] + confuse[1][1];
-    cout << "0\t" << confuse[0][0] << "\t" << confuse[0][1] << "\t" << confuse[0][0]/label0 << "\t" << label0 << endl;
-    cout << "1\t" << confuse[1][0] << "\t" << confuse[1][1] << "\t" << confuse[1][1]/label1 << "\t" << label1 << endl;
-    cout<<"Accuracy:"<<(confuse[0][0]+confuse[1][1])/(label0+label1)<<endl;
-    if(argc == 6) {
-        ifstream test(argv[5]);
-        string line;
-        while(getline(test, line)) {
-            csv_read(line.c_str(), x[0]);
-            cerr << model.binary(x[0]) << endl;
+    double** pred = dmatrix(row, num_classes);
+    for(int j=0;j<num_classes;j++){
+        LR model = v[j];
+        //double** confuse = dmatrix(num_classes, num_classes);
+        for(int i = 0; i < row; ++i) {
+            pred[i][j] = model.binary(x[i]);
+            //int label = (int)y[i];
+            //confuse[label][pred]++;
         }
     }
-    free_matrix(x, row);
-    free_vector(y);
+    double** confuse = dmatrix(num_classes, num_classes);
+    int cnt = 0;
+   
+    for (int i=0;i<row;i++){
+        double mx = 0;
+        int cl = 0;
+        for(int j=0;j<num_classes;j++){
+            if(mx<pred[i][j]){
+                mx = pred[i][j];
+                cl = j;
+                //cout<<j<<" "<<5<<endl;
+            }
+            //cout<<pred[i][j]<<" ";
+        }
+        //cout<<cl<<endl;
+        //cout<<endl;
+        int label = (int)y[i];
+        //cout<<label<<" ";
+        if(label==cl)
+            cnt++;
+        confuse[label][cl]++;
+    }
+    cout<<cnt<<endl;
+    cout<<"Confusion Matrix:"<<endl;
+     cout<<"Labels/Predict"<<endl;
+    cout<<" \t";
+    for(int i=0;i<num_classes;i++)
+        cout<<i<<"\t";
+    cout<<endl;
+    for(int i=0;i<num_classes;i++){
+        cout<<i<<"\t";
+        for(int j=0;j<num_classes;j++){
+            cout<<confuse[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
